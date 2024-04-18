df6258845bebfaed8c25374548df6ac13d35b4df
bdecoute <bart.decoutere@ni.com>
2024-04-18 22:25:53 +0200

    Only load copilot on command :Copilot

--- init.lua
+++ init.lua
@@ -971,8 +971,10 @@ require('lazy').setup({
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
   },
-  -- GitHub Copilot
-  'github/copilot.vim',
+  { -- GitHub Copilot
+    'github/copilot.vim',
+    cmd = 'Copilot',
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
