5622c794c039fc9584f68cf7d3a18c145f359d23
bdecoute <bart.decoutere@ni.com>
2024-04-18 22:04:35 +0200

    install copilot

--- init.lua
+++ init.lua
@@ -844,6 +844,8 @@ require('lazy').setup({
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
   },
+  -- GitHub Copilot
+  'github/copilot.vim',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
