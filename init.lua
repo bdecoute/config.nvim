0f0e11855a807b90d18e8105cd26e85fc6f5f126
bdecoute <bart.decoutere@ni.com>
2024-11-21 11:38:07 +0100

    experiment can be deleted in the future

--- init.lua
+++ init.lua
@@ -119,6 +119,18 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.opt.clipboard = {'unnamedplus'}
+-- vim.opt.clipboard:append {
+--   name= 'WslClipboard',
+--   copy= {
+--     ['+']= 'clip.exe',
+--     ['*']= 'clip.exe',
+--   },
+--   paste= {
+--     ['+']= 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--     ['*']= 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--   },
+--   cache_enabled= 0,
+-- }
 
 -- Enable break indent
 vim.opt.breakindent = true
