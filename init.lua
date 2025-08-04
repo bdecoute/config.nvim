5e7950bc40167457c761a5fb589da32284de71cc
bdecoute <bart.decoutere@emerson.com>
2025-08-04 10:59:10 +0200

    add bash and sh to list of lsp's

--- init.lua
+++ init.lua
@@ -698,6 +698,9 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        bashls = {
+          filetypes = {'sh', 'bash'}
+        },
 
         lua_ls = {
           -- cmd = { ... },
